f5bd3683d2c8c45fc64504dfa10b04b502edabd7
php1301 <19520854@gm.uit.edu.vn>
2021-07-03 05:09:45 +0700

    Fix Cypress Test - next build translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
 	i18n: {
 		locales: ["en", "de", "es", "ar", "he", "zh", "tr"],
-		defaultLocale: "es",
+		defaultLocale: "en",
 		localeDetection: false,
 	},
 	localePath: path.resolve("./public/locales"),
