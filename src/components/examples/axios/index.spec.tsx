ce6913111d46d86a38c9e1b62058ec05df2b2bd7
php1301 <19520854@gm.uit.edu.vn>
2021-07-03 04:16:44 +0700

    Fix Jest test

--- src/components/examples/axios/index.spec.tsx
+++ src/components/examples/axios/index.spec.tsx
@@ -2,13 +2,10 @@ import { AxiosExample } from "./index";
 import "./mocks";
 import { mockResponse } from "./mocks";
 
+import mount from "@test/mount";
 
-import  from "@test/mount";
-
-
-describe("Axios testing with enzyme and nock", () => { 
-  it("renders without crashing", async () => {
-      expect(true).toBeTruthy();
-  });
+describe("Axios testing with enzyme and nock", () => {
+    it("renders without crashing", async () => {
+        expect(true).toBeTruthy();
+    });
 });
-    
