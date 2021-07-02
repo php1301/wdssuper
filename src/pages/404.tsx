779257dbbec9521c688baac34bba827982a8e312
php1301 <19520854@gm.uit.edu.vn>
2021-07-03 04:46:19 +0700

    Fix Cypress Test - isolatedModules tsconfig.json

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,22 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
-import Layout from "@components/layout/layout";
+import { Layout } from "@components/layout/layout";
 
 export default function ErrorPage() {
-	return <h2>Error</h2>;
+    return <h2>Error</h2>;
 }
 
 ErrorPage.Layout = Layout;
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-	return {
-		props: {
-			...(await serverSideTranslations(locale!, [
-				"common",
-				"forms",
-				"menu",
-				"footer",
-			])),
-		},
-	};
+    return {
+        props: {
+            ...(await serverSideTranslations(locale!, [
+                "common",
+                "forms",
+                "menu",
+                "footer",
+            ])),
+        },
+    };
 };
